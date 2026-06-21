4737fc3ce70fbbe017a02def4049c5dbf202d795
Syed Abbuzar <syedabbuzar777@gmail.com>
2026-06-21 16:55:33 +0530

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-//  baseURL: 'https://edu-aura-backend-t1du.vercel.app/api',
-  baseURL: 'http://localhost:5000/api',
+baseURL: 'https://edu-aura-backend-t1du.vercel.app/api',
+
 });
 
 api.interceptors.request.use((config) => {
